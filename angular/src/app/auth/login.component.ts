f81b59aac3090981a2f612e427ccb4be70564b75
Aurélien Matouillot <a.matouillot@gmail.com>
2018-11-19 18:33:37 +0100

    sign in page rendering

--- angular/src/app/auth/login.component.ts
+++ angular/src/app/auth/login.component.ts
@@ -5,21 +5,18 @@ import { AuthService } from './auth.service';
 
 @Component({
   template: `
-  <br>
-  <br>
-  <form (submit)="onSubmit()">
-    <div class="form-group">
-      <label for="exampleInputEmail1">Username</label>
+  <div class="block-signin">
+    <form (submit)="onSubmit()" class="form-signin">
+      <h1 class="h3 mb-3 font-weight-normal text-center">Please sign in</h1>
+      <label for="email" class="sr-only">Username</label>
       <input type="text" class="form-control" required="required"
-        id="exampleInputEmail1" placeholder="Username" name="username" [(ngModel)]="username">
-    </div>
-    <div class="form-group">
-      <label for="exampleInputPassword1">Password</label>
+        id="email" placeholder="Username" name="username" [(ngModel)]="username">
+      <label for="password" class="sr-only">Password</label>
       <input type="password" required="required" class="form-control"
-        id="exampleInputPassword1" placeholder="Password" name="password" [(ngModel)]="password">
-    </div>
-    <button type="submit" class="btn btn-primary">Login</button>
-  </form>
+        id="password" placeholder="Password" name="password" [(ngModel)]="password">
+      <button type="submit" class="btn btn-primary btn-block mt-3">Sign in</button>
+    </form>
+  </div>
   `
 })
 export class LoginComponent {
