7c10c5075a062942b4bc3625835d012e5d9e31a3
Aurélien Matouillot <a.matouillot@gmail.com>
2018-12-19 14:41:52 +0100

    Add error message on error from API

---
+++ angular/src/app/error.interceptor.ts
@@ -0,0 +1,37 @@
+import { Injectable } from '@angular/core';
+import { Router } from '@angular/router';
+import {
+  HttpInterceptor,
+  HttpRequest,
+  HttpHandler,
+  HttpEvent,
+  HttpErrorResponse,
+} from '@angular/common/http';
+
+import { Observable } from 'rxjs';
+import { catchError } from 'rxjs/operators';
+import { _throw } from 'rxjs/observable/throw';
+
+import { MessagesServive } from './messages/messages.service';
+
+
+@Injectable()
+export class ErrorInterceptor implements HttpInterceptor {
+
+  constructor(private router: Router, private messagesService: MessagesServive) {}
+
+  intercept(request: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
+      return next.handle(request).pipe(
+        catchError(response => {
+          if (response instanceof HttpErrorResponse) {
+            if (response.status === 500) {
+              this.messagesService.addError(response.error.error);
+              // TODO: check if we really need to throw the exection here,
+              // perhaps we can stop the propagation
+            }
+          }
+          return _throw(response);
+        })
+      );
+  }
+}
